3fe7f365a4c23ed94ef26fb33d6555ebe2f3e47e
Shane Houghton <shanegh13@gmail.com>
2024-05-17 09:05:39 -0400

    feat/3 - update action.yml

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const TestReporter = require('../src/classes/TestReporter');
 
 const outputFile = process.env.INPUT_OUTPUTFILE ?? 'output.md';
-const coverageDir = process.env.INPUT_COVERAGEDIR ?? './src/**.js';
-const coveragePct = process.env.INPUT_COVERAGEPCT ?? 80;
-const branchesTrue = process.env.INPUT_BRANCHESTRUE ?? false;
+const coverageDir = process.env.INPUT_COVERAGEDIR ?? './**.js';
+const coveragePct = parseInt(process.env.INPUT_COVERAGEPCT) ?? 80;
+const branchesTrue = Boolean(process.env.INPUT_BRANCHESTRUE);
 
 const TR = new TestReporter(outputFile, coverageDir, coveragePct, branchesTrue);
 
